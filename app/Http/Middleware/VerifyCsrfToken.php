fc98ddfeb3c608b64eea312a728da950ed161904
Milica Galic <milicagalicdev@gmail.com>
2022-10-20 12:14:20 +0200

    xx

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,10 +13,8 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        '/login',
-        '/register',
-        '/logout',
-        '/transactions'
+        '*',
+        
 
     ];
 }
