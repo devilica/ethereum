62b5a6c3f0500cec9f22ae6936bcf209873709f9
Milica Galic <milicagalicdev@gmail.com>
2022-10-21 19:35:58 +0200

    removed csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,6 @@
     <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
